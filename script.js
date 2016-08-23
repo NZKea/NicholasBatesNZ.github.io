e86c65bb2c237f96ecf8309d03be9aa9d4949e68
Nicholas Bates <nicholasbates.bates@gmail.com>
2016-08-23 18:25:21 +1200

    Cleaned up

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
+function loadCode(file) {
+    $("#dynamic").load(file);
+}
+
 var selected = "home";
 var fileLocation = "home/home.html";
-var htmlCode = "<p>An error has occured. Please reload the page.</p>";
 
 if (selected === "home") {
     fileLocation = "home/home.html";
 }
 
-function loadCode(file) {
-    $("#dynamic").load(file);
-}
-
 loadCode(fileLocation);
-loadCode("bio.html");
-
-
-//$.get(fileLocation, function(data) {
-//    htmlCode = data;
-//});
-
-//document.getElementById("dynamic").innerHTML = htmlCode;
