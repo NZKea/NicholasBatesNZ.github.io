623221a83aa5c774f7066bc8381eb3a945bc2dbe
Nicholas Bates <nicholasbates.bates@gmail.com>
2016-08-23 18:01:30 +1200

    Reinplemented engine

---
+++ script.js
@@ -0,0 +1,13 @@
+var selected = "home";
+var fileLocation = "home/home.html";
+var htmlCode = "<p>An error has occured. Please reload the page.</p>";
+
+if (selected === "home") {
+    fileLocation = "http://home/home.html";
+}
+
+$.get(fileLocation, function(data) {
+    htmlCode = data;
+});
+
+document.getElementById("dynamic").innerHTML = htmlCode;
