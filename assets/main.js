ecd12b713c30c5871d35bd9f5956de7e4bd969ad
Nicholas Bates <nicholasbates.bates@gmail.com>
2016-08-27 16:28:41 +1200

    Optimisations

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,30 @@
+var fileLocation, mainImage, heading;
+
 function init() {
 
     if (document.getElementById("home").checked) {
         fileLocation = "home/home.html";
         mainImage = "home/hobbiton.jpg";
+        heading = "Blog";
     }
     else if (document.getElementById("projects").checked) {
         fileLocation = "projects/projects.html";
         mainImage = "projects/coding.jpg";
+        heading = "Projects";
     }
     else if (document.getElementById("contact").checked) {
         fileLocation = "contact/contact.html";
-        mainImage = "contact/orchestra.jpg";
+        mainImage = "contact/erebor.jpg";
+        heading = "Contact Me";
     }
     else {
         fileLocation = "error/oobe.html";
         mainImage = "error/Me.jpg";
+        heading = "Error";
     }
     $("#mainImg").attr("src", mainImage)
     $("#dynamic").load(fileLocation);
+    document.getElementById("heading").innerHTML = heading;
 }
 
 window.onload = init();
