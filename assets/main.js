9a1dcb74837dba8d2ccb48f55d3288507fff968c
Nicholas Bates <nicholasbates.bates@gmail.com>
2016-08-27 15:31:37 +1200

    Beautified

--- assets/main.js
+++ assets/main.js
@@ -2,17 +2,21 @@ function init() {
 
     if (document.getElementById("home").checked) {
         fileLocation = "home/home.html";
+        mainImage = "home/hobbiton.jpg";
     }
     else if (document.getElementById("projects").checked) {
         fileLocation = "projects/projects.html";
+        mainImage = "projects/coding.jpg";
     }
     else if (document.getElementById("contact").checked) {
         fileLocation = "contact/contact.html";
+        mainImage = "contact/orchestra.jpg";
     }
     else {
-        fileLocation = "assets/oobe.html";
+        fileLocation = "error/oobe.html";
+        mainImage = "error/Me.jpg";
     }
-
+    $("#mainImg").attr("src", mainImage)
     $("#dynamic").load(fileLocation);
 }
 
